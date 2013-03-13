3e7affbd91e9bd2abbab405ad2b94b89e439b200
Jared Scott <jscott@convertro.com>
2013-03-12 22:47:43 -0700

    Stripping filename and using that as the key for the translation

--- pipeline_i18next/i18next.py
+++ pipeline_i18next/i18next.py
@@ -8,16 +8,15 @@ class I18nextCompiler(SubProcessCompiler):
     output_extension = 'js'
 
     def match_file(self, filename):
-      return filename.endswith('.yml')
+        return filename.endswith('.yml')
 
     def compile_file(self, infile, outfile, outdated=False, force=False):
-      if not outdated and not force:
-        return  # No need to recompiled file
-      # Load the file as a Python Object
-      locale = yaml.load(infile)
-      # Dump object as a JSON representation
-      return "(function(global){" \
-           + "global.Locales = global.Locales || {};" \
-           + "global.Locales." \
-           + "})(window);" \
-           + json.dumps(locale)
+        if not outdated and not force:
+            return  # No need to recompiled file
+        # Load the file as a Python Object
+        translation = yaml.load(infile)
+        # Parse out the name of the locale
+        locale = unicode.rsplit(infile, '/', 1)[1].strip('.yml')
+        # Dump object as a JSON representation
+        return "(function(global){global.Locales = global.Locales || {};global.Locales.%s = { translation: %s}; })(window);".format(
+            locale, json.dumps(translation))
