5108b2fe8247ceb766a68779367eca2572e61562
Jared Scott <jscott@convertro.com>
2013-03-13 09:35:35 -0700

    Adding PIPELINE_I18NEXT_NAMESPACE setting as well as correctly reading and setting output

--- pipeline_i18next/i18next.py
+++ pipeline_i18next/i18next.py
@@ -2,9 +2,9 @@ import yaml
 import json
 from django.conf import settings
 
-from pipeline.compilers import SubProcessCompiler
+from pipeline.compilers import CompilerBase
 
-class I18nextCompiler(SubProcessCompiler):
+class I18nextCompiler(CompilerBase):
     output_extension = 'js'
 
     def match_file(self, filename):
@@ -13,10 +13,26 @@ class I18nextCompiler(SubProcessCompiler):
     def compile_file(self, infile, outfile, outdated=False, force=False):
         if not outdated and not force:
             return  # No need to recompiled file
+        f = open(infile, 'rb')
         # Load the file as a Python Object
-        translation = yaml.load(infile)
+        translation = yaml.load(f)
+        f.close()
         # Parse out the name of the locale
         locale = unicode.rsplit(infile, '/', 1)[1].strip('.yml')
         # Dump object as a JSON representation
-        return "(function(global){global.Locales = global.Locales || {};global.Locales.%s = { translation: %s}; })(window);".format(
-            locale, json.dumps(translation))
+        jsonString = json.dumps(translation)
+
+        # Create Output
+        out =  u'(function(global){{' \
+               u'global.{0} = global.{0} || {{}};' \
+               u'global.{0}.{1} = {{ translation: {2} }};' \
+               u' }})(window);'.format(
+            getattr(settings, 'PIPELINE_I18NEXT_NAMESPACE', 'Locales'),
+            locale,
+            jsonString)
+
+        newFile = open(outfile, 'wb')
+        newFile.write(out)
+        newFile.close()
+        # Compiled
+        return True
